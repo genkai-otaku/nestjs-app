0a1175c1b72157934c5fed985ae8d69faa4c2ead
seino914 <tonosaki914@icloud.com>
2026-01-10 23:37:57 +0900

    cats追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, CatsModule],
   controllers: [],
   providers: [],
 })
