ae2f892f47fa2d3d6bb24726c0afa5c6ba2e1fb5
seino914 <tonosaki914@icloud.com>
2026-01-11 16:58:10 +0900

    prismaClient追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { CatsModule } from './cats/cats.module';
 import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, CatsModule],
+  imports: [AuthModule, UserModule, CatsModule, PrismaModule],
   controllers: [],
   providers: [],
 })
